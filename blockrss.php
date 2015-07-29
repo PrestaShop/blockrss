467eadfc92b89b408d63c266981d2e7d9518be6b
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-07-29 16:45:43 +0200

    // Version updated to v1.3.0

--- blockrss.php
+++ blockrss.php
@@ -47,7 +47,7 @@ class Blockrss extends Module
 		$this->displayName = $this->l('RSS feed block');
 		$this->description = $this->l('Adds a block displaying a RSS feed.');
 
-		$this->version = '1.2.3';
+		$this->version = '1.3.0';
 		$this->author = 'PrestaShop';
 		$this->error = false;
 		$this->valid = false;
