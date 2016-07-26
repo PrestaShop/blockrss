ba0e96c54f5bdb66d1490c3ff7ea8a5ae1021aa9
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-26 18:41:02 +0200

    // Version updated to v1.3.2

--- blockrss.php
+++ blockrss.php
@@ -47,7 +47,7 @@ class Blockrss extends Module
 		$this->displayName = $this->l('RSS feed block');
 		$this->description = $this->l('Adds a block displaying a RSS feed.');
 
-		$this->version = '1.3.1';
+		$this->version = '1.3.2';
 		$this->author = 'PrestaShop';
 		$this->error = false;
 		$this->valid = false;
